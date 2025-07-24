74b803ef6d1711db302e5b9d4b4132f872cda255
ochwada <nakhulolinda@yahoo.com>
2025-07-24 16:33:51 +0200

    added dto package

---
+++ src/main/java/com/ochwada/geomemory_vault/dto/GeoMemoryRequestDTO.java
@@ -0,0 +1,36 @@
+package com.ochwada.geomemory_vault.dto;
+
+
+import lombok.*;
+
+import java.util.*;
+
+/**
+ * *******************************************************
+ * Package: com.ochwada.geomemory_vault.dto
+ * File: GeoMemoryRequestDTO.java
+ * Author: Ochwada
+ * Date: Thursday, 24.Jul.2025, 4:28 PM
+ * Description: DTO used to receive memory creation requests from the client.
+ * - Includes only the fields required from the user.
+ * Objective:
+ * *******************************************************
+ */
+
+@Data
+public class GeoMemoryRequestDTO {
+    private String title;
+
+    private String description;
+
+    private String userId;
+
+    private List<Double> coordinates; // [longitude, latitude]
+
+    private String summary;  // weather summary
+
+    private String temp;     // weather temperature
+
+    // Optional: the client could also send a placeName (but usually derived by server)
+    private String placeName;
+}
---
+++ src/main/java/com/ochwada/geomemory_vault/dto/GeoMemoryResponseDTO.java
@@ -0,0 +1,40 @@
+package com.ochwada.geomemory_vault.dto;
+
+
+import lombok.*;
+
+import java.time.*;
+import java.util.*;
+
+/**
+ * *******************************************************
+ * Package: com.ochwada.geomemory_vault.dto
+ * File: GeoMemoryResponseDTO.java
+ * Author: Ochwada
+ * Date: Thursday, 24.Jul.2025, 4:31 PM
+ * Description: DTO for sending memory data back to the client.
+ * -  Mirrors the structure of GeoMemory but flattens nested fields for clarity.
+ * Objective:
+ * *******************************************************
+ */
+
+@Data
+public class GeoMemoryResponseDTO {
+    private String id;
+
+    private String userId;
+
+    private String title;
+
+    private String description;
+
+    private String placeName;
+
+    private List<Double> coordinates;
+
+    private String summary;
+
+    private String temp;
+
+    private LocalDateTime createdAt;
+}
