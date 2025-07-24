78d5aca538ad47970eb5e966cbdc5208aacea208
ochwada <nakhulolinda@yahoo.com>
2025-07-24 15:18:48 +0200

    first commit - project setup

---
+++ src/main/java/com/ochwada/geomemory_vault/GeoMemoryVaultApplication.java
@@ -0,0 +1,13 @@
+package com.ochwada.geomemory_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoMemoryVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoMemoryVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ochwada/geomemory_vault/GeoMemoryVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ochwada.geomemory_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoMemoryVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
