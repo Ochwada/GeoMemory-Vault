16d44444283e147880a09a0c23353d151ec4936e
ochwada <nakhulolinda@yahoo.com>
2025-07-24 17:38:39 +0200

    added repository package

---
+++ src/main/java/com/ochwada/geomemory_vault/repository/GeoMemoryRepository.java
@@ -0,0 +1,17 @@
+package com.ochwada.geomemory_vault.repository;
+
+
+/**
+ * *******************************************************
+ *  Package: com.ochwada.geomemory_vault.repository
+ *  File: GeoMemoryRepository.java
+ *  Author: Ochwada
+ *  Date: Thursday, 24.Jul.2025, 5:33 PM
+ *  Description: 
+ *  Objective: 
+ * *******************************************************
+ */
+ 
+ 
+ public interface GeoMemoryRepository {
+}
---
+++ src/main/java/com/ochwada/geomemory_vault/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.ochwada.geomemory_vault.repository;
+
+
+/**
+ * *******************************************************
+ *  Package: com.ochwada.geomemory_vault.repository
+ *  File: UserRepository.java
+ *  Author: Ochwada
+ *  Date: Thursday, 24.Jul.2025, 5:30 PM
+ *  Description:  UserRepository provides CRUD operations for the {@link Joke} entity by extending {@link MongoRepository}.
+ *  Objective: 
+ * *******************************************************
+ */
+ 
+ 
+ public class UserRepository {
+}
